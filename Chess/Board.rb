aef17191a9a1b67ef3c8097e27ee5a7d49b2c344
pko912 <kopeter912@gmail.com>
2022-10-04 16:38:53 -0400

    chess

--- Chess/Board.rb
+++ Chess/Board.rb
@@ -1,1 +1,46 @@
-hihihihihi
+require_relative "piece.rb"
+
+class Board
+
+    def initialize()
+        @rows = Array.new(8) {Array.new(8,nil)}
+    end
+
+
+    def [](pos)
+        row,col = pos
+        @rows[row][col]
+    end
+
+    def []=(pos,value)
+        row,col = pos
+        @rows[row][col] = value
+    end
+
+
+    def move_piece(start_pos, end_pos)
+        # piece = Piece.new()
+    end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class NullPiece < Piece
+
+#     def initialize(start_pos = nil)
+#         @start_pos = start_pos
+
+#     end
+
+# end
